19c03c8d599f76d1561511d65cb018c9f1336013
Kfir Stri <kfirstar@gmail.com>
2014-09-02 23:22:00 +0300

    A lot of stuff

--- Scripts/roomies.js
+++ Scripts/roomies.js
@@ -3,8 +3,23 @@ var app = angular.module('roomiesApp', [
   "mobile-angular-ui",
 ]);
   
+  
+app.factory('getMessagesFactory', function ($http) {
+  
+  var database = new ODatabase('http://localhost:2480/Roomies');
+  database.open("admin", "admin");
+  var queryResult = database.query("select from Message order by publish_date_time desc", null, '*:-1');
+  
+  return queryResult;
+});
+  
 app.config(function($routeProvider) {
-    $routeProvider.when('/', { templateUrl: 'Templates/home.html' });
-    $routeProvider.when('/bills', { templateUrl: 'Templates/home.html' });
-    $routeProvider.when('/billsPay', { templateUrl: 'Templates/home.html' });
+    $routeProvider.when('/', { templateUrl: 'Templates/messages.html' });
+    $routeProvider.when('/bills', { templateUrl: '' });
+    $routeProvider.when('/billsPay', { templateUrl: '' });
+});
+  
+
+app.controller('MessagesController', function($scope, $http, getMessagesFactory) {
+  $scope.messages = getMessagesFactory.result;
 });
